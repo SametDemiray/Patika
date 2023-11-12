5a3c956a17d3648cae6e3233052b00d3188dfd13
Samet Demiray <sametdemiray@yahoo.com>
2023-11-12 20:01:45 +0300

    echo "# Patika" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/SametDemiray/Patika.git git push -u origin main

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik Ders Notu : ");
+        mat = input.nextInt();
+
+        System.out.print("Fizik Ders Notu : ");
+        fizik = input.nextInt();
+
+        System.out.print("Kimya Ders Notu : ");
+        kimya = input.nextInt();
+
+        System.out.print("Türkçe Ders Notu : ");
+        turkce = input.nextInt();
+
+        System.out.print("Tarih Ders Notu : ");
+        tarih = input.nextInt();
+
+        System.out.print("Muzik Ders Notu : ");
+        muzik = input.nextInt();
+
+        Integer toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+
+        double sonuc = toplam / 6;
+
+        System.out.println(sonuc >= 60 ? "Geçtiniz." : "Kaldınız!");
+        System.out.println("Not Ortalamanız : " + sonuc);
+
+    }
+}
