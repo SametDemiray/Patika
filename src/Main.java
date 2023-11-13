86d5a8eee1425aa92759e533b42c151fce4002e9
Samet Demiray <sametdemiray@yahoo.com>
2023-11-13 08:34:26 +0300

    git branch -m master Begining git fetch origin git branch -u origin/Begining Begining git remote set-head origin -a

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double tutar, toplam;
+        double kdv = 1.18;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Fiyat Giriniz : ");
+        tutar = input.nextInt();
+
+
+        toplam = (tutar * kdv);
+
+        double sonuc = toplam;
+
+        System.out.println(toplam);
+        System.out.println("Genel Toplam : " + sonuc);
+
+    }
+}
